064ee540bb2b34559338e728b8c424a67d48baa9
rodolphe <rdelapierre@warren-walter.com>
2024-01-24 15:45:04 +0100

    wtf

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ let cells = [];
 	for ( let i = 0; i < 200; i++ ) {
 		cells[i] = [];
 		for ( let j = 0; j < 200; j++ ) {
-			cells[i][j] = Math.round( Math.random()*0.6 );
+			cells[i][j] = Math.random().toFixed(2);
 			if ( cells[i][j] ) {
-				ctx.fillStyle = 'black';
+				ctx.fillStyle = `hsl(${cells[i][j]*60}, 100%, ${cells[i][j]*100}%)`;
 				ctx.fillRect( i, j, 1, 1 );
 			}
 		}
@@ -26,6 +26,13 @@ let cells = [];
 	run();
 } )();
 
+function gaussian( value ) {
+    let curve_center = 4;
+    let curve_width = 0.88;
+
+    return Math.exp( -Math.pow( parseFloat( ( value ).toFixed( 2 ) ) - curve_center, 2 ) / ( 2 * Math.pow( curve_width, 2 ) ) );
+}
+
 function run() {
 
 	const cells_copy = JSON.parse( JSON.stringify( cells ) );
@@ -33,24 +40,19 @@ function run() {
 	for ( let i = 0; i < 200; i++ ) {
 		for ( let j = 0; j < 200; j++ ) {
 
-			let neighbors = 0;
-			neighbors += cells_copy[i==199?0:i+1][j];
-			neighbors += cells_copy[i==0?199:i-1][j];
-			neighbors += cells_copy[i][j==199?0:j+1];
-			neighbors += cells_copy[i][j==0?199:j-1];
-			neighbors += cells_copy[i==199?0:i+1][j==199?0:j+1];
-			neighbors += cells_copy[i==0?199:i-1][j==0?199:j-1];
-			neighbors += cells_copy[i==199?0:i+1][j==0?199:j-1];
-			neighbors += cells_copy[i==0?199:i-1][j==199?0:j+1];
+			let neighbors_value = 0;
+				neighbors_value += parseFloat( cells_copy[i==199?0:i+1][j] );
+				neighbors_value += parseFloat( cells_copy[i==0?199:i-1][j] );
+				neighbors_value += parseFloat( cells_copy[i][j==199?0:j+1] );
+				neighbors_value += parseFloat( cells_copy[i][j==0?199:j-1] );
+				neighbors_value += parseFloat( cells_copy[i==199?0:i+1][j==199?0:j+1] );
+				neighbors_value += parseFloat( cells_copy[i==0?199:i-1][j==0?199:j-1] );
+				neighbors_value += parseFloat( cells_copy[i==199?0:i+1][j==0?199:j-1] );
+				neighbors_value += parseFloat( cells_copy[i==0?199:i-1][j==199?0:j+1] );
 
-			if ( cells_copy[i][j] ) {
-				if ( neighbors < 2 || neighbors > 3 ) { cells[i][j] = false; }
-				else if ( neighbors == 2 || neighbors == 3 ) { cells[i][j] = true; }
-			} else {
-				if ( neighbors == 3 ) { cells[i][j] = true; }
-			}
+			cells[i][j] = gaussian( neighbors_value );
 
-			ctx.fillStyle = cells[i][j] ? 'black' : 'white';
+			ctx.fillStyle = `hsl(${cells[i][j]*60}, 100%, ${cells[i][j]*100}%)`;
 			ctx.fillRect( i, j, 1, 1 );
 
 		}
